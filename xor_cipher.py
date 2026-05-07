da6b314d3cb2b1b1563b72f513a2dd8207aaeb7d
Amaac20 <amandial2011@gmail.com>
2026-05-07 14:42:56 -0500

    Update xor_cipher.py

--- xor_cipher.py
+++ xor_cipher.py
@@ -2,16 +2,14 @@ texto = input("Ingrese un texto: ")
 
 llave = "Uide2026"
 
-
 def xor_stream(texto, llave):
-
     resultado = ""
 
     for i in range(len(texto)):
 
         letra = ord(texto[i])
 
-        bit_llave = int(llave[i % len(llave)])
+        bit_llave = ord(llave[i % len(llave)])
 
         cifrado = letra ^ bit_llave
 
@@ -22,11 +20,9 @@ def xor_stream(texto, llave):
 
 print("Llave:", llave)
 
-
 textoCifrado = xor_stream(texto, llave)
-
 print("Texto cifrado:", textoCifrado)
 
-textoDescifrado = xor_stream(textoCifrado, llave)
 
+textoDescifrado = xor_stream(textoCifrado, llave)
 print("Texto descifrado:", textoDescifrado)
