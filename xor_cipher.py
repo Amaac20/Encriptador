6e56d049ac1c2bc3c7c6aad2530a7e53cb4b1cdf
Amaac20 <amandial2011@gmail.com>
2026-05-07 14:40:46 -0500

    Add files via upload

---
+++ xor_cipher.py
@@ -0,0 +1,32 @@
+texto = input("Ingrese un texto: ")
+
+llave = "Uide2026"
+
+
+def xor_stream(texto, llave):
+
+    resultado = ""
+
+    for i in range(len(texto)):
+
+        letra = ord(texto[i])
+
+        bit_llave = int(llave[i % len(llave)])
+
+        cifrado = letra ^ bit_llave
+
+        resultado += chr(cifrado)
+
+    return resultado
+
+
+print("Llave:", llave)
+
+
+textoCifrado = xor_stream(texto, llave)
+
+print("Texto cifrado:", textoCifrado)
+
+textoDescifrado = xor_stream(textoCifrado, llave)
+
+print("Texto descifrado:", textoDescifrado)
